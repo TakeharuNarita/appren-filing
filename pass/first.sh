454b87c7651d4d71dbe5cb994587ad023235c7d2
takegongon <t995771oxff@gmail.com>
2023-04-30 14:38:24 +0900

    Auto push by .sh

--- pass/first.sh
+++ pass/first.sh
@@ -3,16 +3,25 @@
 read -ra SERVICE <<< "sv"
 # SERVICE="sv"
 CURRENT=$(cd $(dirname $0);pwd)
-  MATCHS=$(grep "^$SERVICE:.\+:.\+$" "$CURRENT/password.txt")
 
-  if [[ -n "$MATCHS" ]]; then
-    echo "$MATCHS" | while IFS= read -r LINE; do
-      IFS=':' read -ra FIELDS <<< "$LINE"
-      echo "グループ名: ${FIELDS[0]}"
-      echo "ユーザー名: ${FIELDS[1]}"
-      echo "パスワード: ${FIELDS[2]}"
-      echo "---------------------------"
-    done
-  else
-    echo 'そのサービスは登録されていません。'
-  fi
+MATCHS=$(grep "^$SERVICE:.\+:.\+$" "$CURRENT/password.txt")
+
+echo "$MATCHS" | while IFS= read -r TEST
+do
+  echo "$TEST" | while IFS=":" read -r WD
+  do
+    echo "$WD"
+  done
+done
+
+#   echo "$MATCHS" | while IFS= read -r $INaaaE; do
+#     echo "$LINE"
+#     IFS=':' read -ra FIELDS <<< "$LINE"
+#     echo "グループ名: ${FIELDS[0]}"
+#     echo "ユーザー名: ${FIELDS[1]}"
+#     echo "パスワード: ${FIELDS[2]}"
+#     echo "---------------------------"
+#   done
+# else
+#   echo 'そのサービスは登録されていません。'
+# fi
