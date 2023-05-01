3acd34dc93d47047884c516d957d39ba2fd609bd
takegongon <t995771oxff@gmail.com>
2023-05-01 20:52:31 +0900

    Auto push by .sh

---
+++ pass/only_gpg.sh
@@ -0,0 +1,18 @@
+function c () {
+  gpg -c test.txt
+
+  if [ -f test.txt.gpg ]; then
+    echo "test.txt.gpgが存在します。"
+  else
+    echo "test.txt.gpgが存在しません。"
+    echo "test.txt.gpgを作成します。"
+    gpg -c test.txt
+  fi
+}
+
+function d () {
+   TEXT=$(gpg --decrypt test.txt.gpg)
+   echo $TEXT
+}
+
+d
