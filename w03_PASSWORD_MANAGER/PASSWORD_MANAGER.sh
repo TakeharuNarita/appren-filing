399e3dc946910d92af21d77c7b09d8d0fdecb34f
takegongon <t995771oxff@gmail.com>
2023-05-02 05:11:59 +0900

    ak

--- w03_PASSWORD_MANAGER/PASSWORD_MANAGER.sh
+++ w03_PASSWORD_MANAGER/PASSWORD_MANAGER.sh
@@ -2,76 +2,93 @@ function main(){
   ATTEMPT=0
   PW_GPG=`dirname "$0"`"/password.gpg"
   echo "パスワードマネージャーへようこそ！"
-  Vset_phrase
-  Vhome
+  v_set_phrase
+  v_home
   echo "Thank you!"
   exit 0
 }
 
-function Vset_phrase(){
+function v_set_phrase(){
   [[ $ATTEMPT > 2 ]] && echo -e "\033[31m試行回数が上限に達しました。\033[0m" && exit 1
-  read -sp "パスフレーズを入力してください：" PHRASE; [ -t 0 ]
-  Vphrase_check
+  read -sp "パスフレーズを入力してください：" PHRASE;
+  v_phrase_check
 }
 
-function Vphrase_check(){
+function v_phrase_check(){
   if [ -f $PW_GPG ]; then
-    gpg -q -d --batch --yes --passphrase "${PHRASE}" "${PW_GPG}" 2>/dev/null || Vattempt
+    # gpg -q -d --batch --yes --passphrase "${PHRASE}" "${PW_GPG}" 2>/dev/null || Vattempt
+    # gpg -q -d --batch --yes --passphrase "${PHRASE}" "${PW_GPG}" >/dev/null 2>&1 || v_attempt
+    # gpg -q -d --batch --yes --passphrase "${PHRASE}" "${PW_GPG}" 1> /dev/null 2> err
+    output=$(gpg -q -d --batch --yes --passphrase "${PHRASE}" "${PW_GPG}" 2> >(read -d '' err_output))
+    echo $err_output
+    if [ -s "$err_output" ]; then
+      err
+    else
+      hilyo
+    fi
+
   else
     echo "" | gpg -q -c --batch --yes --passphrase "${PHRASE}" --output "${PW_GPG}"
     [ -t 0 ] && echo -e "\n'password.gpg'が存在しないため、新たなパスワードファイルを作成しました。"
   fi
 }
 
-function Vattempt(){
+function hilyo(){
+  echo "hilyo"
+}
+function err(){
+  echo "err"
+}
+
+function v_attempt(){
   echo -e "\033[31m\ngpg Error: 誤ったパスフレーズを入力した可能性があります。\033[0m"
   ATTEMPT=`expr $ATTEMPT + 1`
-  Vset_phrase
+  v_set_phrase
 }
 
-function Vhome(){
+function v_home(){
   read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" option;
-  Vloop $option
+  v_loop $option
 }
 
-function Vloop() {
+function v_loop() {
   option=$1
   case $option in
     "Add Password"|"a"|"A")
-      Vadd_password
+      v_add_password
       ;;
     "Get Password"|"g"|"G")
-      Vget_password
+      v_get_password
       ;;
     "Exit"|"e"|"E")
       ;;
     *)
       read -p "入力が間違えています。Add Password/Get Password/Exit から入力してください。" option;
-      Vloop $option
+      v_loop $option
       ;;
   esac
 }
 
-function Vadd_password() {
+function v_add_password() {
   read -p "サービス名を入力してください：" SERVICE; [ -t 0 ] && echo
   read -p "ユーザー名を入力してください：" USER; [ -t 0 ] && echo
   read -sp "パスワードを入力してください：" PASS; [ -t 0 ] && echo
   
-  Bgpg_add "${SERVICE}:${USER}:${PASS}"
+  b_gpg_add "${SERVICE}:${USER}:${PASS}"
   echo 'パスワードの追加は成功しました。'
-  Vhome
+  v_home
 }
 
-function Vget_password() {
+function v_get_password() {
   echo
 }
 
-function Sgpg_read () {
+function s_gpg_read () {
    TEXT=$(gpg -d test.txt.gpg)
    echo $TEXT
 }
 
-function Bgpg_add () {
+function b_gpg_add () {
   line=$1
   exist=$(gpg -q -d --batch --yes --passphrase "${PHRASE}" "${PW_GPG}")
   echo -e "${line}\n${exist}" | gpg -q -c --batch --yes --passphrase "${PHRASE}" --output ${PW_GPG}
