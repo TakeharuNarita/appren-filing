8d6c61c8059a2c78bbd9eb524d8e7b6986148b24
takegongon <t995771oxff@gmail.com>
2023-05-02 08:19:41 +0900

    refactoring

--- w03_PASSWORD_MANAGER/PASSWORD_MANAGER.sh
+++ w03_PASSWORD_MANAGER/PASSWORD_MANAGER.sh
@@ -1,66 +1,71 @@
+#!/bin/bash
+
 function main(){
   ATTEMPT=0
   GPG_FILE=`dirname "$0"`"/password.gpg"
   echo "パスワードマネージャーへようこそ！"
-  v_set_phrase
-  v_home
-  echo "Thank you!"
-  exit 0
+  set_phrase
+  home
 }
 
-function v_set_phrase(){
-  [[ $ATTEMPT > 2 ]] && echo -e "\033[31m試行回数が上限に達しました。\033[0m" && exit 1
+function set_phrase(){
+  [ $ATTEMPT -gt 2 ] && echo -e "\033[31m試行回数が上限に達しました。\033[0m" && exit 1
   read -sp "パスフレーズを入力してください：" PHRASE;
-  v_phrase_check
+  phrase_check
 }
 
-function v_phrase_check(){
+function phrase_check(){
   if [ -f $GPG_FILE ]; then
     gpg -q -d --batch --yes --passphrase "${PHRASE}" "${GPG_FILE}" > /dev/null 2> /dev/null
-    status=$?; echo; [[ $status != 0 ]] && v_attempt
+    status=$?; echo; [[ $status != 0 ]] && attempt
   else
     echo "" | gpg -q -c --batch --yes --passphrase "${PHRASE}" --output "${GPG_FILE}"
     [ -t 0 ] && echo -e "\n'password.gpg'が存在しないため、新たなパスワードファイルを作成しました。"
   fi
 }
 
-function v_attempt(){
+function attempt(){
   echo -e "\033[31mgpg Error: 誤ったパスフレーズを入力した可能性があります。\033[0m"
-  ATTEMPT=`expr $ATTEMPT + 1`
-  v_set_phrase
+  ATTEMPT=$(expr $ATTEMPT + 1)
+  set_phrase
 }
 
-function v_home(){
-  read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" option;
-  while [ true ]; do
-    case $option in
-      "Add Password"|"a"|"A")
-        v_add_password
-        read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" option;
-        ;;
-      "Get Password"|"g"|"G")
-        v_get_password
-        read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" option;
-        ;;
-      "Exit"|"e"|"E")
-        break
-        ;;
-      *)
-        read -p "入力が間違えています。Add Password/Get Password/Exit から入力してください。" option;
-        ;;
-    esac
+function home(){
+  while true; do
+    read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" option;
+    select_option "${option}"
   done
 }
 
-function v_add_password() {
+@param $1: オプション文字列
+function select_option(){
+  option=$1
+  case $option in
+    "Add Password"|"a"|"A")
+      add_password
+      ;;
+    "Get Password"|"g"|"G")
+      get_password
+      ;;
+    "Exit"|"e"|"E")
+      echo "Thank you!"
+      exit 0
+      ;;
+    *)
+      echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+      ;;
+  esac
+}
+
+function add_password() {
   read -p "サービス名を入力してください：" service
   read -p "ユーザー名を入力してください：" user
   read -sp "パスワードを入力してください：" pass
   
-  v_gpg_add "${service}:${user}:${pass}"
+  gpg_add "${service}:${user}:${pass}"
 }
 
-function v_get_password() {
+function get_password() {
   exist=$(gpg -q -d --batch --yes --passphrase "${PHRASE}" "${GPG_FILE}")
   read -p "サービス名を入力してください：" service
   matchs=$(echo "${exist}" | grep "^${service}:.\+:.\+$")
@@ -76,7 +81,7 @@ function v_get_password() {
 }
 
 # @param $1: 追加する行の文字列、改行なし
-function v_gpg_add () {
+function gpg_add () {
   line=$1
   exist=$(gpg -q -d --batch --yes --passphrase "${PHRASE}" "${GPG_FILE}")
   echo -e "${line}\n${exist}" | gpg -q -c --batch --yes --passphrase "${PHRASE}" --output "${GPG_FILE}"
